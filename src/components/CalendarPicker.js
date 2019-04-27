a3548482d2a543e5ab7ae997b381e0f346ddf623
Mecladon <Mecladon@github.com>
2019-04-27 12:00:46 +0100

    CalendarPicker and DropDownMenue

---
+++ src/components/CalendarPicker.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import CalendarPicker from 'react-native-calendar-picker';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedStartDate: null,
+    };
+    this.onDateChange = this.onDateChange.bind(this);
+  }
+
+  onDateChange(date) {
+    this.setState({
+      selectedStartDate: date,
+    });
+  }
+  render() {
+    const { selectedStartDate } = this.state;
+    const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+	const maxDate = new Date(2030,1,1);
+	const minDate = new Date(2019,1,1);
+    return (
+      <View style={styles.container}>
+        <CalendarPicker
+          onDateChange={this.onDateChange}
+		  minDate={minDate}
+		  maxDate={maxDate}
+        />
+
+        <View>
+          <Text>SELECTED DATE:{ startDate }</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    marginTop: 100,
+  },
+});
