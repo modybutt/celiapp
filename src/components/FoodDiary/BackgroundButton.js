4319eb1321068c64a560793f62478baaabe8b31a
THeLenchen <mra7219@students.fhv.at>
2019-06-22 12:58:47 +0200

    ddd

---
+++ src/components/FoodDiary/BackgroundButton.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { TouchableOpacity, View, Text, StyleSheet, Image } from 'react-native'
+export default class BackgroundButton extends React.Component {
+
+  render() {
+    const styles = this.makeStyles()
+    return (
+      <TouchableOpacity style={styles.touchable} onPress={this.props.onPress}>
+        <View style={styles.view}>
+          {this.makeImageIfAny(styles)}
+          <Text style={styles.text}>{this.props.title}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+makeImageIfAny(styles) {
+    if (this.props.showImage) {
+      return <Image style={styles.image} source={require('../../assets/images/check.png')} />
+    }
+  }
+
+makeStyles() {
+    return StyleSheet.create({
+      view: {
+        flexDirection: 'row',
+        borderRadius: 23,
+        borderColor: this.props.borderColor,
+        borderWidth: 2,
+        backgroundColor: this.props.backgroundColor,
+        height: 46,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingLeft: 16,
+        paddingRight: 16
+      },
+      touchable: {
+        marginLeft: 4,
+        marginRight: 4,
+        marginBottom: 8
+      },
+      image: {
+        marginRight: 8
+      },
+      text: {
+        fontSize: 18,
+        textAlign: 'center',
+        color: this.props.textColor,
+        fontSize: 16
+      }
+    })
+  }
+}
