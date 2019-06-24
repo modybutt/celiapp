eba3990704271537901b3fbf43681112305a192f
THeLenchen <mra7219@students.fhv.at>
2019-06-24 22:33:12 +0200

    fsd

--- src/components/FoodDiary/FoodDiaryImageEdit.js
+++ src/components/FoodDiary/FoodDiaryImageEdit.js
@@ -22,7 +22,7 @@ export default class FoodDiaryImageEdit extends React.Component {
     }
 
     render() {
-        if (this.active == true) {
+        if (this.active == null || this.active == true) {
             if (this.state.snapshot == null) {
                 return (
                     <TouchableOpacity style={styles.container} onPress={() => this.props.navigation.navigate('Camera', {cb: (pic) => this.onPictureTaken(pic)})}>
