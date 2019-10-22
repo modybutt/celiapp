8806259e777aee504613daf4fe8969e9c7f6f725
modybutt <45882806+modybutt@users.noreply.github.com>
2019-10-22 20:23:54 +0200

    unsupported UDP

--- src/screens/GearScreen.js
+++ src/screens/GearScreen.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { View, StyleSheet, } from 'react-native';
-import MenuButton from '../components/MenuButton';
-import EntryList from '../components/EntryList';
 import LanguageManager from '../manager/LanguageManager';
 
 export default class GearScreen extends React.Component {
@@ -9,9 +7,32 @@ export default class GearScreen extends React.Component {
     title: LanguageManager.getInstance().getText("GEAR"),
   });
 
-  render() {
+  componentDidMount() {
+    //var dgram = require('dgram')
+    // OR, if not shimming via package.json "browser" field:
+    var dgram = require('react-native-udp')
+    var HOST = "192.168.1.125";
+    var PORT = 8080;
 
+    var socket = dgram.createSocket('udp4')
     
+    socket.once('listening', function() {
+      var buf = toByteArray('excellent!')
+      socket.send(buf, 0, buf.length, remotePort, remoteHost, function(err) {
+        if (err) throw err
+
+        console.log('message was sent')
+      })
+    })
+
+    socket.on('message', function(msg, rinfo) {
+      console.log('message was received', msg)
+    })
+
+    socket.bind(HOST, PORT)
+  }
+
+  render() {
     return (
       <View style={styles.container}>
         
