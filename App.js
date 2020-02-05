b0a09dd0f9a8a1261733b328e129e2f042cf0bb3
Ken Power <github@kenpower.com>
2020-02-05 15:32:26 +0000

    get username dialog

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import GlutonManager from './src/manager/GlutonManager';
 import GearManager from './src/manager/GearManager';
 import UploadManager from './src/manager/UploadManager';
 import Notifiationmanager from './src/manager/NotificationManager';
+import UsernameDialog from './src/components/UsernameDialog';
 
 import { } from 'react-native-dotenv';
 
@@ -53,14 +54,29 @@ export default class App extends React.Component {
       }
     );
     
-    this.setState({isSplashReady: true});
+    this.setState({
+      isSplashReady: true,
+      hasToken: !!settings.nickname
+    });
     setTimeout(() =>  this.setState({isAppReady: true}), 3000);
   }
+
+  
+  handleNewUsername = (e) => {
+    //TODO get new auth token
+    //TODO persist user name to DB
+    this.setState({hasToken: true})
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        {this.state.isSplashReady == false ? null : <AppNavigator/>}
+        {this.state.isSplashReady == false 
+          ? null 
+          : this.state.hasToken 
+            ? <AppNavigator/>
+            : <UsernameDialog onUsername ={this.handleNewUsername}/>
+        }
         <LoadingScreen hide={this.state.isAppReady} style={styles.loading}/>
       </View>
     );
