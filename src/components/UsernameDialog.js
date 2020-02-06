dfb601e12718d9ace09de4841a48e596492abef6
Ken Power <github@kenpower.com>
2020-02-06 09:33:34 +0000

    Add username dialog

---
+++ src/components/UsernameDialog.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  View,
+  Dimensions,
+  Text
+} from 'react-native';
+import Dialog from "react-native-dialog";
+
+export default class UsernameDialog extends React.Component {
+
+  render() {
+    return (
+      <View style={{width: Dimensions.get('window').width, height: Dimensions.get('window').height, justifyContent: 'space-around', alignItems: 'center'}}>
+          <View></View>
+              <Dialog.Container visible={true}>
+              <Dialog.Title>Username</Dialog.Title>
+              <Dialog.Description>
+                Please enter your username.
+              </Dialog.Description>
+              <Dialog.Input></Dialog.Input>
+              <Dialog.Button label={"OK"} onPress = {this.props.onUsername}/>
+            </Dialog.Container>
+      </View>
+    );
+  }
+}
