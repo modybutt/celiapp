e06cfaeb1405fc957185449c23eb6a480aefaea9
Ken Power <github@kenpower.com>
2020-03-10 22:48:00 +0000

    Refactor CircleDisplay
    Make it into a display only component, does not have any knowledge
    of dates or events

--- src/components/CircleDisplayFull.js
+++ src/components/CircleDisplayFull.js
@@ -1,5 +1,5 @@
-import React,{Suspense} from 'react';
-import {View, Text} from 'react-native';
+import React, { Suspense } from 'react';
+import { View, Text } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
 import SymptomCircle from './CircleDisplay';
 import DatabaseManager from '../manager/DatabaseManager';
@@ -8,84 +8,89 @@ import DatabaseManager from '../manager/DatabaseManager';
 let m_dateStack = [];
 
 export default class CircleDisplayFull extends React.Component {
-  
+
   updateData() {
     const self = this;
 
     let maxDateValue = new Date().getUTCDate()
+    m_dateStack.length = 0;
 
-    for (let i = 0; i <= maxDateValue; i++)
-    {
-      m_dateStack.push(0);
-    }
+    // for (let i = 0; i <= maxDateValue; i++)
+    // {
+    //   m_dateStack.push(0);
+    // }
 
-    DatabaseManager.getInstance().fetchEvents(
-      new Date().UTC,
-      (_, error) => {alert(error)}, 
-      (_, {rows: { _array }}) => (
-        _array.forEach(element => {
-          if( (new Date (element.created).getUTCMonth()) == new Date().getUTCMonth()
-           && (new Date (element.created).getUTCFullYear()) == new Date().getUTCFullYear()
-           && (new Date (element.created).getUTCDate()) <= maxDateValue)
-           {
-              if (element.eventType == 3)
-              {
-                if (element.objData[10] == 0)
-                { //gluten
-                  m_dateStack[new Date (element.created).getUTCDate()] = 1;
-                }
-                else if (element.objData[10] == 1)
-                { //no gluten
-                  m_dateStack[new Date (element.created).getUTCDate()] = 2;
-                }
-              }
-              else if (element.eventType == 0)
-              {  // only symptoms
-                if (m_dateStack[new Date (element.created).getUTCDate()] == 0)
-                {
-                    m_dateStack[new Date (element.created).getUTCDate()] = 3;
-                }
-              }
-           }
-        }),
+    // DatabaseManager.getInstance().fetchEvents(
+    //   new Date().UTC,
+    //   (_, error) => {alert(error)}, 
+    //   (_, {rows: { _array }}) => (
+    //     _array.forEach(element => {
+    //       if( (new Date (element.created).getUTCMonth()) == new Date().getUTCMonth()
+    //        && (new Date (element.created).getUTCFullYear()) == new Date().getUTCFullYear()
+    //        && (new Date (element.created).getUTCDate()) <= maxDateValue)
+    //        {
+    //           if (element.eventType == 3)
+    //           {
+    //             if (element.objData[10] == 0)
+    //             { //gluten
+    //               m_dateStack[new Date (element.created).getUTCDate()] = 1;
+    //             }
+    //             else if (element.objData[10] == 1)
+    //             { //no gluten
+    //               m_dateStack[new Date (element.created).getUTCDate()] = 2;
+    //             }
+    //           }
+    //           else if (element.eventType == 0)
+    //           {  // only symptoms
+    //             if (m_dateStack[new Date (element.created).getUTCDate()] == 0)
+    //             {
+    //                 m_dateStack[new Date (element.created).getUTCDate()] = 3;
+    //             }
+    //           }
+    //        }
+    //     }),
 
-        self.setState({data: _array})
-      )
-    )
+    //     self.setState({data: _array})
+    //   )
+    // )
+
+
+
+    console.log('m_dateStack :', JSON.stringify(m_dateStack));
+
+    self.setState({ data: m_dateStack})
   }
 
-  componentDidMount()
-  {
+  componentDidMount() {
     this.updateData();
   }
 
 
   render() {
-   
+
     return (
       <View>
         <NavigationEvents
           onDidFocus={() => this.updateData()}
         />
-      { this.state && this.state.data &&
+        {this.state && this.state.data &&
 
-        <SymptomCircle colourInfoList={m_dateStack}
-              progBarX={this.props.progBarX}
-              progBarY={this.props.progBarY}
-              progBarBorderWidth={this.props.progBarBorderWidth}
-              progBarBigRadius={this.props.progBarBigRadius}
-              progBarSmallRadius={this.props.progBarSmallRadius}
-              NoInputColour={this.props.NoInputColour} 
-              GlutenColour={this.props.GlutenColour} 
-              NoGlutenColour={this.props.NoGlutenColour} 
-              SymptomOnlyColour={this.props.SymptomOnlyColour} 
-              FinishedBackGroundColour={this.props.FinishedBackGroundColour}
-              UnfinishedBackGroundColour={this.props.UnfinishedBackGroundColour}
-              InsideCircleBGColour={this.props.InsideCircleBGColour}
-              />
-      }
+          <SymptomCircle colourInfoList={m_dateStack}
+            progBarX={this.props.progBarX}
+            progBarY={this.props.progBarY}
+            progBarBorderWidth={this.props.progBarBorderWidth}
+            progBarBigRadius={this.props.progBarBigRadius}
+            progBarSmallRadius={this.props.progBarSmallRadius}
+            FinishedBackGroundColour={this.props.FinishedBackGroundColour}
+            UnfinishedBackGroundColour={this.props.UnfinishedBackGroundColour}
+            InsideCircleBGColour={this.props.InsideCircleBGColour}
+            maxNumberOfInnerDots={31}
+            numberOfInnerDots={m_dateStack.length}
+            titleText={"Day 10"}
+          />
+        }
       </View>
 
     )
-  }  
+  }
 }
