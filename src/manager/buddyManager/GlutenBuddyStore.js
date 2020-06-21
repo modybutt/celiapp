05db10e2adbe2654f97e1820c76a1b6c3851df87
lpr95 <30443524+lpr95@users.noreply.github.com>
2020-06-21 11:39:41 +0200

    introduced Avatar for IOS; resolved bug: 'mark avatar items on startup'

--- src/manager/buddyManager/GlutenBuddyStore.js
+++ src/manager/buddyManager/GlutenBuddyStore.js
@@ -1,68 +1,42 @@
 import { observable, action } from "mobx";
 import { create, persist } from "mobx-persist";
-import { AsyncStorage } from "react-native";
+import { AsyncStorage, Platform } from "react-native";
 
 const hydrate = create({
   storage: AsyncStorage,
 });
 
-class GlutenBuddyStore {
-  @observable title = "Hello World2";
+class GlutenBuddyStore {  
 
-  // default values:
-  @observable id = 0;
-  @observable size = 200;
-  @observable avatarStyle = "Transparent";
-  @observable topType = "LongHairBob";
+// default values:
+@observable size = 200;
+@observable avatarStyle = Platform.OS === 'ios' ? "Circle" : "Transparent";
+@observable topType = "ShortHairShortCurly";
+@observable accessoriesType = "Blank"; // categoryGlasses
+@observable hairColor = "Black";
+@observable facialHairType = "Blank"; // e.g. MoustacheMagnum; BeardMedium -> Not for teenagers;
+@observable clotheType = "CollarSweater";
+@observable clotheColor = "PastelBlue";
+@observable skinColor = "Tanned";
 
-  @observable accessoriesType = "Blank"; // No Glasses
-  @observable hairColor = "BrownDark";
-  @observable facialHairType = "Blank"; // MoustacheMagnum; BeardMedium
-  @observable clotheType = "Hoodie";
-  @observable clotheColor = "PastelBlue";
-  @observable eyeType = "Default";
-  @observable eyebrowType = "Default";
-  @observable mouthType = "Smile";
-  @observable skinColor = "Brown";
+@observable activeTopTypeId = 20011;
+@observable activeAccessoriesTypeId = 30048;
+@observable activeHairColorId = 50063;
+@observable activeFacialHairTypeId = 0;
+@observable activeClotheTypeId = 10002;
+@observable activeClotheColorId = 10002;
+@observable activeSkinColorId = 40055;
 
-  @observable activeTopTypeId = 0;
-  @observable activeAccessoriesTypeId = 0;
-  @observable activeHairColorId = 0;
-  @observable activeFacialHairTypeId = 0;
-  @observable activeClotheTypeId = 0;
-  @observable activeClotheColorId = 0;
-  @observable activeEyeTypeId = 0;
-  @observable activeEyebrowTypeId = 0;
-  @observable activeMouthTypeId = 0;
-  @observable activeSkinColorId = 0;
-
-  // semi persistent points:
-  @observable score = 0;
-  @observable nextLevel = 1000;
-  @observable currentLevel = 1;
-  // for progress bar:
-  @observable thisLevelBegin = 0;
-  @observable thisLevelEnd = 1000;
-  @observable progressBarProgress = 0.0;
-
-  //@observable tempAvatarChange = [0, "itemId", "color"];
+// semi persistent points:
+@observable score = 0;
+@observable nextLevel = 1000;
+@observable currentLevel = 1;
+// for progress bar:
+@observable thisLevelBegin = 0;
+@observable thisLevelEnd = 1000;
+@observable progressBarProgress = 0.0;
 
 
-  @action setId = (id) => {
-    this.id = id;
-  };
-  //@observable
-  @action onChangeAvatarItem = (val) => {
-    this.title = val;
-    console.log("store: called with:", val);
-    //this.forceUpdate();
-  };
-
-  //@action setTempAvatarChange = (sectionId, itemId, color = "Blue02") => {
-    //this.tempAvatarChange[0] = sectionId;
-    //this.tempAvatarChange[1] = itemId;
-    //this.tempAvatarChange[2] = color;
-  //};
   @action
   setTopType = (value) => {
     this.topType = value;
@@ -88,22 +62,9 @@ class GlutenBuddyStore {
     this.clotheColor = value;
   };
   @action
-  setEyeType = (value) => {
-    this.eyeType = value;
-  };
-  @action
-  setEyebrowType = (value) => {
-    this.eyebrowType = value;
-  };
-  @action
-  setMouthType = (value) => {
-    this.mouthType = value;
-  };
-  @action
   setSkinColor = (value) => {
     this.skinColor = value;
   };
-
   @action
   setActiveTopTypeId = (value) => {
     this.activeTopTypeId = value;
@@ -129,32 +90,9 @@ class GlutenBuddyStore {
     this.activeClotheColorId = value;
   };
   @action
-  setActiveEyeTypeId = (value) => {
-    this.activeEyeTypeId = value;
-  };
-  @action
-  setActiveEyebrowTypeId = (value) => {
-    this.activeEyebrowTypeId = value;
-  };
-  @action
-  setActiveMouthTypeId = (value) => {
-    this.activeMouthTypeId = value;
-  };
-  @action
   setActiveSkinColorId = (value) => {
     this.activeSkinColorId = value;
   };
-
-  @observable activeTopTypeId = 0;
-  @observable activeAccessoriesTypeId = 0;
-  @observable activeHairColorId = 0;
-  @observable activeFacialHairTypeId = 0;
-  @observable activeClotheTypeId = 0;
-  @observable activeClotheColorId = 0;
-  @observable activeEyeTypeId = 0;
-  @observable activeEyebrowTypeId = 0;
-  @observable activeMouthTypeId = 0;
-  @observable activeSkinColorId = 0;
 
   @action
   setScore = (value) => {
@@ -186,8 +124,6 @@ class GlutenBuddyStore {
 }
 
 const schema = {
-  id: true,
-  title: true,
   size: true,
   avatarStyle: true,
   topType: true,
@@ -196,29 +132,20 @@ const schema = {
   facialHairType: true,
   clotheType: true,
   clotheColor: true,
-  eyeType: true,
-  eyebrowType: true,
-  mouthType: true,
   skinColor: true,
 
-  // which item ID is currentry active?:
   activeTopTypeId: true,
   activeAccessoriesTypeId: true,
   activeHairColorId: true,
   activeFacialHairTypeId: true,
   activeClotheTypeId: true,
   activeClotheColorId: true,
-  activeEyeTypeId: true,
-  activeEyebrowTypeId: true,
-  activeMouthTypeId: true,
   activeSkinColorId: true,
 };
 
 const store = new GlutenBuddyStore();
 
 const persistingStore = persist(schema)(store);
-hydrate("id", persistingStore);
-hydrate("title", persistingStore);
 hydrate("size", persistingStore);
 hydrate("avatarStyle", persistingStore);
 hydrate("topType", persistingStore);
@@ -227,9 +154,6 @@ hydrate("hairColor", persistingStore);
 hydrate("facialHairType", persistingStore);
 hydrate("clotheType", persistingStore);
 hydrate("clotheColor", persistingStore);
-hydrate("eyeType", persistingStore);
-hydrate("eyebrowType", persistingStore);
-hydrate("mouthType", persistingStore);
 hydrate("skinColor", persistingStore);
 hydrate("score", persistingStore);
 hydrate("currentLevel", persistingStore);
