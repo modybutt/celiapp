604990ca081934bb5ccea6ef49e25e2fdf23e66d
Jur van Oerle <j.h.van.oerle@hva.nl>
2021-04-01 16:25:28 +0200

    Create dynamic color passing to ImageHeader

--- src/screens/ImageHeader.js
+++ src/screens/ImageHeader.js
@@ -4,8 +4,11 @@ import React from 'react'
 import Color from '../constants/Colors'
 import { Image, StyleSheet, View, Text } from 'react-native'
 
-export default class ImageHeader extends React.Component {
-    render() {
+export default class ImageHeader extends React.Component 
+{
+	render() 
+	{
+		const styles = this.getStyleSheet(this.props.color);
         return (
             <View style={styles.container}>
                 <Image
@@ -19,27 +22,29 @@ export default class ImageHeader extends React.Component {
             </View>
         )
     }
+
+	getStyleSheet(color)
+	{
+		return StyleSheet.create({
+			container: {
+				flex: 1,
+				flexDirection: 'row',
+				justifyContent: 'center',
+				alignItems: 'center',
+			},
+		
+			image: {
+				width: 25,
+				height: 25,
+				tintColor: color,
+				zIndex: 999
+			},
+		
+			text: {
+				fontSize: 15,
+				color: color,
+				marginHorizontal: 5
+			}	
+		});
+	}
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-    image: {
-        width: 25,
-        height: 25,
-		tintColor: Color.tintColor,
-        zIndex: 999
-    },
-
-    text: {
-        fontSize: 15,
-		color: Color.tintColor,
-		marginHorizontal: 5
-    }
-
-})
