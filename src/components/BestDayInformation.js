0f0e46334598cc2fa3a1124ec83f3fc9f5b1c16a
Ken Power <github@kenpower.com>
2021-06-29 11:03:42 +0100

    change best day tool tip text

--- src/components/BestDayInformation.js
+++ src/components/BestDayInformation.js
@@ -23,16 +23,7 @@ export default class BestDayInformation extends React.Component {
                               onLayout={event => {
                                   const layout = event.nativeEvent.layout;
                                   console.log("layouttxt:", layout)}}>
-                            Your BEST DAY is calculated by adding your entries times what you have entered in each entry.
-                        </Text>
-                        <Text style={styles.text}>
-                            For symptoms: 1, 2, 3 points for mild, moderate, severe  respectively and 5 for no symptom
-                        </Text>
-                        <Text style={styles.text}>
-                            Food: & GIP: 3 points for no gluten, 1 point no idea, 0 points for Gluten.
-                        </Text>
-                        <Text style={styles.text}>
-                            Energy: 1 to 5 with 1 for low energy emoiji and 5 high energy level emoiji
+                            Your BEST DAY is calculated based on how well you are felling and how close you get to meeting you daily goals.
                         </Text>
                     </View>
 
